a64f5faa5ab478cc3c8402a89f51cfaf7a6a61e8
lairkers <lars.vieting@gmx.de>
2025-01-19 08:25:35 +0100

    Some renamings and restructuring of solver. First attempt of closed groups (WIP)

--- solver.rb
+++ solver.rb
@@ -1,4 +1,4 @@
-# Like in chess the fields are named:
+# Like in chess the cells are named:
 #   A B C D E F G H I
 # 1
 # 2
@@ -38,7 +38,7 @@ class Solver
       possibilities = field.reject { |p| p.last.size == 1 }
       lowest_num_of_possibilities = possibilities.map { |p| p.last.size }.min
       g = possibilities.find { |p| p.last.size == lowest_num_of_possibilities }
-      possible_guesses = g.last.map { |num| { :fields => [g.first], :number => num } }
+      possible_guesses = g.last.map { |num| { :cells => [g.first], :number => num } }
 
       #puts('Intermediate result')
       #puts(field_str)
@@ -65,29 +65,68 @@ class Solver
 
   private
 
+  def init_statements
+    (1..9).each do |number|
+      # Rows
+      (1..9).each do |row|
+        cells = %w[A B C D E F G H I].map { |c| "#{c}#{row}"}
+        @statements << { :cells => cells, :number => number }
+      end
+      # Columns
+      %w[A B C D E F G H I].each do |column|
+        cells = %w[1 2 3 4 5 6 7 8 9].map { |r| "#{column}#{r}"}
+        @statements << { :cells => cells, :number => number }
+      end
+      # Blocks
+      [%w[A B C], %w[D E F], %w[G H I]].each do |rows|
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9]].each do |columns|
+          cells = []
+          rows.each do |row|
+            columns.each do |column|
+              cells << "#{row}#{column}"
+            end
+          end
+          @statements << { :cells => cells, :number => number }
+        end
+      end
+    end
+  end
+
+  def read_statements_from_file(filename)
+    File.readlines(filename, chomp: true).each_with_index do |line, row_minus_one|
+      line.chars.first(9).each_with_index do |number_as_str, column_as_int|
+        next if number_as_str == ' '
+        number = number_as_str.to_i
+        column = %w[A B C D E F G H I][column_as_int]
+        row = row_minus_one + 1
+        @statements << { :cells => ["#{column}#{row}"], :number => number }
+      end
+    end
+  end
+
   def field
     field = (1..9).map { |row| %w[A B C D E F G H I].map { |col| "#{col}#{row}"}}
                   .flatten
-                  .map { |f| [f, @statements.select { |s| s[:fields].include?(f) }.map { |s| s[:number] }.uniq]}
+                  .map { |f| [f, @statements.select { |s| s[:cells].include?(f) }.map { |s| s[:number] }.uniq]}
     # TODO: nicer representation as hash instead of first/last
     return field
   end
 
-  def field_array
+  def field_as_array
     map = Array.new(9){Array.new(9)}
-    statements = @statements.select { |s| s[:fields].size == 1 }
+    statements = @statements.select { |s| s[:cells].size == 1 }
     statements.each do |statement|
-      field = statement[:fields].first
-      column_index = %w[A B C D E F G H I].find_index(field.chars.first)
-      row_index = field.chars.last.to_i - 1
-      raise "Found duplicate in #{statement[:fields].first}: Current #{map[row_index][column_index]}, new #{statement[:number]}" unless map[row_index][column_index] == nil
+      cell = statement[:cells].first
+      column_index = %w[A B C D E F G H I].find_index(cell.chars.first)
+      row_index = cell.chars.last.to_i - 1
+      raise "Found duplicate in #{statement[:cells].first}: Current #{map[row_index][column_index]}, new #{statement[:number]}" unless map[row_index][column_index] == nil
       map[row_index][column_index] = statement[:number]
     end
     return map
   end
 
   def field_str
-    return field_array.map { |row| row.map { |c| c.nil? ? ' ' : c }.join }
+    return field_as_array.map { |row| row.map { |c| c.nil? ? ' ' : c }.join }
   end
 
   def complete?
@@ -95,7 +134,7 @@ class Solver
   end
 
   def correct?
-    f = field_array
+    f = field_as_array
     # Rows
     (0..8).each do |row|
       (1..9).each do |number|
@@ -119,106 +158,113 @@ class Solver
     return true
   end
 
-  def init_statements
-    (1..9).each do |number|
-      # Rows
-      (1..9).each do |row|
-        fields = %w[A B C D E F G H I].map { |c| "#{c}#{row}"}
-        @statements << { :fields => fields, :number => number }
-      end
-      # Columns
-      %w[A B C D E F G H I].each do |column|
-        fields = %w[1 2 3 4 5 6 7 8 9].map { |r| "#{column}#{r}"}
-        @statements << { :fields => fields, :number => number }
-      end
-      # Blocks
-      [%w[A B C], %w[D E F], %w[G H I]].each do |rows|
-        [[1, 2, 3], [4, 5, 6], [7, 8, 9]].each do |columns|
-          fields = []
-          rows.each do |row|
-            columns.each do |column|
-              fields << "#{row}#{column}"
-            end
-          end
-          @statements << { :fields => fields, :number => number }
-        end
-      end
-    end
-  end
-
-  def read_statements_from_file(filename)
-    File.readlines(filename, chomp: true).each_with_index do |line, row_minus_one|
-      line.chars.first(9).each_with_index do |number_as_str, column_as_int|
-        next if number_as_str == ' '
-        number = number_as_str.to_i
-        column = %w[A B C D E F G H I][column_as_int]
-        row = row_minus_one + 1
-        @statements << { :fields => ["#{column}#{row}"], :number => number }
-      end
-    end
+  def statement_hash
+    return @statements.map { |s| s[:cells] }.flatten.size
   end
 
   def solve_linear
-    num_statements = @statements.map { |s| s[:fields] }.flatten.size + 1
-    while num_statements > @statements.map { |s| s[:fields] }.flatten.size
-      num_statements = @statements.map { |s| s[:fields] }.flatten.size
-      linear_step
+    while true
+      while true
+        fingerprint = statement_hash
+        reduce_statements_by_containment
+        reduce_statements_by_single_numbers
+        break if fingerprint == statement_hash
+
+        raise "Linear steps lead to invalid solution." unless correct?
+      end
+
+      fingerprint = statement_hash
+      #reduce_statements_by_closed_groups
+      break if fingerprint == statement_hash
     end
   end
 
-  def linear_step
+  def reduce_statements_by_containment
     @statements.each do |needle|
       # In case any of the steps below already marked this needle as to be deleted
-      next if needle[:fields].empty?
+      next if needle[:cells].empty?
 
-      # Contained means: needle has same number and fields is included in statement's fields
+      # Contained means: needle has same number and cells is included in statement's cells
       contained_statements = @statements.select { |s| s[:number] == needle[:number] }
-                                        .select { |s| needle[:fields].all? { |f| s[:fields].include?(f) } }
+                                        .select { |s| needle[:cells].all? { |f| s[:cells].include?(f) } }
                                         .reject { |s| s == needle }
 
-      if needle[:fields].size == 1
-        # 2. If unique: Delete the field from all other @statements' fields
-        @statements.select { |s| s[:fields].include?(needle[:fields].first) }
+      # 0. If unique: Delete the cell from all other @statements' cells
+      if needle[:cells].size == 1
+        @statements.select { |s| s[:cells].include?(needle[:cells].first) }
                     .reject { |s| s == needle }
-                    .each { |s| s[:fields].reject! { |f| f == needle[:fields].first } }
+                    .each { |s| s[:cells].reject! { |f| f == needle[:cells].first } }
       end
 
       # If needle is contained in a statement:
       next if contained_statements.empty?
 
-      # 0. For this number, remove all fields in other statements
+      # 1. For this number, remove all cells in other statements
       contained_statements.each do |cs|
-        fields_to_remove = cs[:fields].reject { |f| needle[:fields].include?(f) }
+        cells_to_remove = cs[:cells].reject { |f| needle[:cells].include?(f) }
         @statements.select { |s| s[:number] == cs[:number] }
                    .reject { |s| s == cs }
                    .reject { |s| s == needle }
-                   .reject { |s| s[:fields].size == 1 }
+                   .reject { |s| s[:cells].size == 1 }
                    .each { |s|
-                      s[:fields].reject! { |f| fields_to_remove.include?(f) }
+                      s[:cells].reject! { |f| cells_to_remove.include?(f) }
                 }
       end
 
-      # 1. Mark the whole contained_statement to be deleted
-      contained_statements.each { |cs| cs[:fields] = [] }
+      # 2. Mark the whole contained_statement to be deleted
+      contained_statements.each { |cs| cs[:cells] = [] }
     end
 
     # Clean up all statements that are empty after the previous step
-    @statements.reject! { |s| s[:fields].empty? }
+    @statements.reject! { |s| s[:cells].empty? }
     @statements.uniq!
+  end
 
-    # Find single numbers per field
-    single_number_per_fields = []
+  def reduce_statements_by_single_numbers
+    # Find single numbers per cell
+    single_number_per_cells = []
     @statements.each do |needle|
-      needle[:fields].each do |field|
-        next if @statements.any? { |s| s[:fields].include?(field) && s[:number] != needle[:number] }
+      needle[:cells].each do |cell|
+        next if @statements.any? { |s| s[:cells].include?(cell) && s[:number] != needle[:number] }
         
-        single_number_per_fields << { :fields => [field], :number => needle[:number] }
+        single_number_per_cells << { :cells => [cell], :number => needle[:number] }
       end
     end
-    @statements.concat(single_number_per_fields)
+    @statements.concat(single_number_per_cells)
     @statements.uniq!
+  end
 
-    raise "Linear steps lead to invalid solution." unless correct?
+  # WIP
+  def reduce_statements_by_closed_groups
+    # Closed group handling
+    groups = []
+    @statements.each do |statement|
+      group = @statements.select{ |s| s[:cells] == statement[:cells] }
+      next if group.nil? || group.size <= 1 || group.map { |s| s[:number] }.uniq.size != group[0][:cells].size
+
+      groups << { :cells => group[0][:cells], :numbers => group.map{ |g| g[:number]} }
+    end
+    puts("GROUPS")
+    groups.uniq!
+    groups.each do |group|
+      puts('group')
+      puts(group)
+    end
+    groups.each do |group|
+      cells_to_remove = group[:cells]
+      group[:numbers].each do |number|
+        @statements.select { |s| s[:number] == number }
+                   .reject { |s| s[:cells] == group[:cells] }
+                   .each { |s|
+                     puts "Removing #{cells_to_remove} because of group #{group} from #{s}"
+                     s[:cells].reject! { |f| cells_to_remove.include?(f) }
+                }
+      end
+    end
+
+    # Clean up all statements that are empty after the previous step
+    @statements.reject! { |s| s[:cells].empty? }
+    @statements.uniq!
   end
 
 end
